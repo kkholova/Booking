cc98f8a0f17b4fb5486ec20d7093869bb9919659
kkholova <k.kholova@godeltech.com>
2021-06-22 18:21:43 +0300

    innitial commit

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasePage {
+
+
+
+}
---
+++ src/test/java/steps/BookingSearchSteps.java
@@ -0,0 +1,11 @@
+package steps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class BookingSearchSteps {
+
+
+}
