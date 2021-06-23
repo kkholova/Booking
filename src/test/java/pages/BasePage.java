ec364f668e23a5b7169ade3d6e18b5f6bf7bbae7
kkholova <k.kholova@godeltech.com>
2021-06-23 11:06:26 +0300

    commit

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,11 +1,4 @@
 package pages;
 
-import io.cucumber.java.Before;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 public class BasePage {
-
-
-
 }
